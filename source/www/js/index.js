5d7cf27209f7714362e1ce2261f7242052dbfa0e
Nick Hulley <nick.hulley98@gmail.com>
2019-12-10 17:12:03 +1100

    Implemented Cordova pause and resume events

--- source/www/js/index.js
+++ source/www/js/index.js
@@ -1,26 +1,8 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
+
 let list;
 var app = {
     // Application Constructor
     initialize: function () {
-
         if (typeof (Storage) === "undefined") {
             $("main").html("Sorry, you can't use this app without local storage...");
             return null;
@@ -54,6 +36,8 @@ var app = {
         attachEvents();
 
         document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
+        document.addEventListener("pause", this.onPause.bind(this), false);
+        document.addEventListener("resume", this.onResume.bind(this), false);
 
     },
 
@@ -62,7 +46,6 @@ var app = {
     // Bind any cordova events here. Common events are:
     // 'pause', 'resume', etc.
     onDeviceReady: function () {
-        this.receivedEvent('deviceready');
     },
 
     // // Update DOM on a Received Event
@@ -76,6 +59,18 @@ var app = {
 
     //     console.log('Received Event: ' + id);
     // }
+
+    
+    onPause: function () {
+        if ($("#blinder").length <= 0)
+            $("body").append(`<div id="blinder" class="faded">Paused</div>`);
+
+    },
+
+    onResume: function () {
+        $("#blinder").html("Welcome back");
+        $("#blinder").fadeOut(1000, function() { $(this).remove(); })
+    },
 
 };
 
